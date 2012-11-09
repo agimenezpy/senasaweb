596b7a6b5012c8a15a8747b4feccfdabfb0a72ea
Alberto Gimenez <albergimenez@gmail.com>
2012-11-08 21:38:43 -0300

    CORRECCION DE FORMULARIOS AL VALIDAR CAMPOS CON CONTENIDO

--- producto/forms.py
+++ producto/forms.py
@@ -7,29 +7,32 @@ from parametros.models import Localidad,Distrito
 class ObraForm(ModelForm):
     def clean_localidad(self):
         localidad = self.cleaned_data['localidad']
-        distrito = self.cleaned_data['distrito']
-        if localidad.distrito_id != distrito.codigo:
-            raise ValidationError("La localidad no pertenece al distrito")
+        if self.cleaned_data.has_key('distrito'):
+            distrito = self.cleaned_data['distrito']
+            if localidad.distrito_id != distrito.codigo:
+                raise ValidationError("La localidad no pertenece al distrito")
         return localidad
 
     def clean(self):
         cleaned_data = super(ObraForm, self).clean()
         coordenadax = self.cleaned_data['coordenada_x']
         coordenaday = self.cleaned_data['coordenada_y']
-        distrito = self.cleaned_data['distrito']
-        if coordenadax == 0 and coordenaday == 0:
-            return cleaned_data
-        pt = Point(coordenadax,coordenaday,srid=32721)
-        if not distrito.geom.contains(pt):
-            raise ValidationError("Las coordenadas X e Y no estan dentro del distrito elegido")
+        if self.cleaned_data.has_key('distrito'):
+            distrito = self.cleaned_data['distrito']
+            if coordenadax == 0 and coordenaday == 0:
+                return cleaned_data
+            pt = Point(coordenadax,coordenaday,srid=32721)
+            if not distrito.geom.contains(pt):
+                raise ValidationError("Las coordenadas X e Y no estan dentro del distrito elegido")
+        return cleaned_data
 
     def clean_ubicacion(self):
         ubicacion = self.cleaned_data['ubicacion']
-        distrito = self.cleaned_data['distrito']
-        if not ubicacion:
-            return ubicacion
-        if not distrito.geom.contains(ubicacion):
-            raise ValidationError("El punto escogido no esta dentro del distrito elegido")
+        if self.cleaned_data.has_key('distrito'):
+            distrito = self.cleaned_data['distrito']
+            if not distrito.geom.contains(ubicacion):
+                raise ValidationError("El punto escogido no esta dentro del distrito elegido")
+        return ubicacion
 
     class Meta:
         model = Obra
